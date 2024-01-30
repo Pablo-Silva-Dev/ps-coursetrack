1d716e43cde9575c1ece6ad146fde844456c12d2
Pablo Silva <pablolucio_@hotmail.com>
2024-01-30 17:10:50 -0300

    chore: added expo-fonts configuration

--- App.tsx
+++ App.tsx
@@ -2,18 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { light } from "@themes/light";
+import {
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from "@expo-google-fonts/roboto";
+import { useFonts } from "expo-font";
 
 export default function App() {
   const { theme: lightTheme } = light;
 
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </ThemeProvider>
-  );
+  const [fontsLoaded] = useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  } else {
+    return (
+      <ThemeProvider theme={lightTheme}>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ThemeProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
