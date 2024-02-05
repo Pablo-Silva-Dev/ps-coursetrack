19cf125ce9967d50996c917dd5ea56d45c679f9d
Pablo Silva <pablolucio_@hotmail.com>
2024-02-05 07:21:54 -0300

    feat: MaskedTextInput component created

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,18 @@
 import { BorderlessButton } from '@components/Buttons/BorderlessButton';
 import { GoogleSignInButton } from '@components/Buttons/GoogleSignInButton';
 import { PrimaryButton } from '@components/Buttons/PrimaryButton';
+import { MaskedTextInput } from '@components/Inputs/MaskedTextInput';
 import { TextInput } from '@components/Inputs/TextInput';
 import { Subtitle } from '@components/Typography/Subtitle';
 import { Text } from '@components/Typography/Text';
 import { Title } from '@components/Typography/Title';
+import { useState } from 'react';
+import { Masks } from 'react-native-mask-input';
 import { Container } from './styles';
 
 export function InitialScreen() {
+  const [val, setVal] = useState('');
+
   return (
     <Container>
       <GoogleSignInButton />
@@ -15,8 +20,14 @@ export function InitialScreen() {
       <BorderlessButton title="test" />
       <Title content="Title" />
       <Subtitle content="Subtitle" />
-      <Text content="Text" />
+      <Text content={val} />
       <TextInput label="Test" />
+      <MaskedTextInput
+        mask={Masks.BRL_CPF}
+        label="CPF"
+        value={val}
+        onChangeText={setVal}
+      />
     </Container>
   );
 }
